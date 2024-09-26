8a0673a26d41b237087cb6e1e902dc724b07e1af
JiSukh <jsukharom1999@gmail.com>
2024-09-26 16:11:19 +0100

    added file structure and new routers

--- app.py
+++ app.py
@@ -5,7 +5,29 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.render_template("index.html")
+    return flask.render_template("home/index.html")
+
+@app.route('/account/')
+def account():
+    return flask.render_template("accounts/account.html")
+
+@app.route('/login/')
+def login():
+    return flask.render_template("accounts/login.html")
+    
+@app.route('/')
+def posts():
+    return flask.render_template("posts/posts.html")
+
+@app.route('/')
+def update():
+    return flask.render_template("posts/update.html")
+
+@app.route('/')
+def security():
+    return flask.render_template("security/security.html")
+
+
     
 
 
