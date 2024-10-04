b791416bea7e16835a965404774dac9bfff0a015
JiSukh <jsukharom1999@gmail.com>
2024-10-04 09:50:53 +0100

    created post creating

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, url_for, redirect
+import config
+from posts.forms import PostForm
 
 
 
 posts_bp = Blueprint('posts', __name__, template_folder='templates')
 
 
-@posts_bp.route('/create/')
+@posts_bp.route('/create/', methods=('GET', 'POST'))
 def create():
-    return render_template("posts/create.html")
+    form = PostForm()
+
+    if form.validate_on_submit():
+        new_post = config.Post(title=form.title.data, body=form.body.data)
+
+        config.db.session.add(new_post)
+        config.db.session.commit()
+
+        flash('Post created', category='success')
+        return redirect(url_for('posts.posts'))
+
+    return render_template('posts/create.html', form=form)
     
 @posts_bp.route('/posts')
 def posts():
